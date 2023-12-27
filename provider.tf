204c4ba33ce3ee1a7da04be94847fab64d289eac
gopal <gopal1409@gmail.com>
2023-12-27 16:33:54 +0530

    module with git

--- provider.tf
+++ provider.tf
@@ -19,7 +19,10 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
+provider "aws" {
+  alias = "us-east-2"
+  region = "us-east-2"
+}
 resource "random_pet" "this" {
   length = 2
 }
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "my_vpc" {
-  source = "./modules/vpc"
+  source = "github.com/gopal1409/my-vpc-module"
   cidr_vpc = var.cidr_vpc
   tags_vpc = var.tags_vpc
 }
+
